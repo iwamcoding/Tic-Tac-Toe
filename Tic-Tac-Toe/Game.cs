fa532b2ba1e9c894011bd0339a4361b5b952345a
Muaviyah Arsalan <muaviyah.arsalan@outlook.com>
2023-12-22 22:58:20 +0300

    Created the game

---
+++ Tic-Tac-Toe/Game.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Tic_Tac_Toe
+    {
+        private readonly char[] locations = new char[9];
+        public char[] Locations { get { return locations; } }
+
+        public Tic_Tac_Toe() { }
+        public Tic_Tac_Toe(char[] locations) { this.locations = locations; }
+
+        public void InitiateNewGame()
+        {
+            for (int i = 0; i < 9; i++) 
+            {
+                if (i > 5)
+                    locations[i] = ' ';
+                else
+                    locations[i] = '_';
+            }
+        }      
+        
+        public void InsertUserInput(char input, int index) 
+        {            
+            if (index < 0 || index > 8)
+                throw new ArgumentOutOfRangeException(nameof(index));
+
+            if (locations[index] != '_' || locations[index] != ' ')
+                locations[index] = input;
+            else
+                throw new ArgumentException($"{nameof(input)} is already filled"); 
+        }        
+
+        public bool AreLocationsFilled()
+        {
+            var filled = true;
+            for (int i = 0;i < 9;i++)
+            {
+                if (IsLocationEmpty(i))
+                {
+                    filled = false;
+                    break;
+                }                    
+            }
+            
+            return filled;
+        }
+
+        private bool IsLocationEmpty(int index)
+        {
+            if (locations[index] == '_' || locations[index] == ' ')
+                return true;
+            else return false;
+        }
+
+        public char? DetermineWin()
+        {
+            char? CheckHorizontally()
+            {
+                char? inputWon = null;
+                for (int i = 0; i < 3; i++)
+                {
+                    var index = i * 3;
+
+                    var rowIsFilled = !(IsLocationEmpty(index)  || IsLocationEmpty(index + 1) || IsLocationEmpty(index + 2));
+
+                    if (rowIsFilled)
+                    {
+                        var allThreeAreSame = locations[index] == locations[index + 1] && locations[index + 1] == locations[index + 2];
+                        if (allThreeAreSame)
+                        {
+                            inputWon = locations[index];
+                        }
+                    }                    
+                }
+
+                return inputWon;
+            }
+
+            char? CheckVertically()
+            {
+                char? inputWon = null;
+                for (int i = 0; i < 3; i++)
+                {
+                    var collumnIsFilled = !(IsLocationEmpty(i) || IsLocationEmpty(i + 3)  || IsLocationEmpty(i + 6));
+
+                    if (collumnIsFilled)
+                    {
+                        var allThreeAreSame = locations[i] == locations[i + 3] && locations[i + 3] == locations[i + 6];
+                        if (allThreeAreSame)
+                            inputWon = locations[i];
+                    }
+                }
+
+                return inputWon;                
+            }
+
+            char? CheckDiagonally()
+            {
+                char? inputWon = null;
+                if (locations[0] == locations[4] && locations[4] == locations[8])
+                    inputWon = locations[0];
+                else
+                    if (locations[2] == locations[4] && locations[4] == locations[6])
+                        inputWon = locations[2];
+
+                return inputWon;
+            }
+
+            char? inputWon = CheckHorizontally();
+            
+            if (inputWon != null)
+                return inputWon;
+
+            inputWon = CheckVertically();
+
+            if (inputWon != null)
+                return inputWon;
+
+            inputWon = CheckDiagonally();
+
+            return inputWon;
+        }
+
+
+    }
+}
--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -1,2 +1,79 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Game;
+
+
+var game = new Tic_Tac_Toe();
+game.InitiateNewGame();
+void RenderTicTacToe()
+{
+    for (int j = 0; j < 3; j++)
+    {
+        var index = j * 3;
+        Console.Write(game.Locations[index]);
+        Console.Write("|");
+        Console.Write(game.Locations[index + 1]);
+        Console.Write("|");
+        Console.WriteLine(game.Locations[index + 2]);
+    }
+}
+
+var movesLeft = false;
+char? playerWon = null;
+var gameFinished = false;
+var playerTurn = 'O';
+var playerInput = 0;
+
+void TakeUserInput()
+{
+    bool inputTaken = false;
+    while(!inputTaken)
+    {
+        Console.Clear();
+        RenderTicTacToe();
+
+        Console.Write($"{playerTurn}'s Turn: ");
+        
+        if (int.TryParse(Console.ReadLine(), out playerInput))
+        {
+            if (playerInput > 0 && playerInput < 10)
+                inputTaken = true;
+            else
+            {
+                Console.WriteLine("Error! Number must be between 0 and 9. Press any key.");
+                Console.ReadKey();
+            }                
+        }        
+    }
+
+}
+
+do
+{
+    Console.Clear();
+    RenderTicTacToe();
+
+    TakeUserInput();
+
+    game.InsertUserInput(playerTurn, playerInput - 1);
+
+    var gameFilled = game.AreLocationsFilled();
+    playerWon = game.DetermineWin();
+    movesLeft = !gameFilled;
+
+    if (!movesLeft || playerWon != null)
+        gameFinished = true;
+
+    if (!gameFinished)
+        if (playerTurn == 'O')
+            playerTurn = 'X';
+        else
+            playerTurn = 'O';
+
+} while (!gameFinished);
+
+if (playerWon != null)
+    Console.WriteLine($"{playerTurn} Won!");
+else
+    Console.WriteLine("No spaces left");
+
+
